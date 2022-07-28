9bcc6ac767f5938276757661c4b6219d4c612616
Leonardo Klinger <gamefivebr@gmail.com>
2022-07-28 17:37:22 -0300

    Removendo classe inutil

--- teste.js
+++
@@ -1,7 +0,0 @@
-const soap = require("soap")
-
-soap.createClient("http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso?WSDL", function (err, client) {
-    client.CapitalCity({ sCountryISOCode: '' }, function (err, result) {
-        console.log(result)
-    })
-})
